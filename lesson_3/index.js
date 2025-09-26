1afca234de129057f003280314333fd8f37c3723
Igor <ihor.diiak.fi.2022@lpnu.ua>
2025-09-26 22:54:00 +0300

    first js task

---
+++ lesson_3/index.js
@@ -0,0 +1,19 @@
+
+// ДЗ 3.1
+let str = "Chicken";
+console.log(typeof(str));
+
+const num = 99;
+console.log(typeof(num));
+
+var age;
+console.log(typeof(age));
+
+const isAdult = true;
+console.log(typeof(isAdult));
+
+let count = null;
+console.log(typeof(count));
+
+let zero = null;
+console.log(zero);
