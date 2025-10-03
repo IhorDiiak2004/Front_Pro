2d5b015dafe025611f8963f9cf835bbec11ace4d
Igor <ihor.diiak.fi.2022@lpnu.ua>
2025-10-03 11:23:50 +0300

    Dz 4.2

--- lesson_4/index.js
+++ lesson_4/index.js
@@ -3,6 +3,7 @@
 let userName = prompt("Як тебе звати?");
 alert(`Hello, ${userName}! How are you?`);
 
+
 // Дз 4.4
 let numOrStr = prompt('input number or string');
 console.log(numOrStr);
@@ -22,6 +23,21 @@ switch (true) {
         break;
 }
 
+
 // Дз 4.2
+let number = prompt("Введіть тризначне число:");
+
+let a = number[0];
+let b = number[1];
+let c = number[2];
+
+if (a === b && b === c) {
+    console.log("Усі цифри однакові");
+} else if (a === b || b === c || a === c) {
+    console.log("Серед цифр є однакові");
+} else {
+    console.log("Усі цифри різні");
+}
+
 
 
