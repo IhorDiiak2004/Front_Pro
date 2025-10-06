225d0d251a06f93d1a932f1b194b20f4a79bec58
Igor <ihor.diiak.fi.2022@lpnu.ua>
2025-10-07 02:15:37 +0300

    update Dz 5.1

--- lesson_5/index.js
+++ lesson_5/index.js
@@ -4,10 +4,14 @@
 let result = "";
 
 for (let i = 20; i <= 30; i += 0.5) {
-  result += `${i} `;
+  result += i;
+  if (i < 30) {
+    result += " ";
+  }
 }
 
 console.log(result);
+
 
 
 // Дз 5.2
