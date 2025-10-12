81cdd7a00ee9deb90d6fa8ed541c69c7c961914f
Igor <ihor.diiak.fi.2022@lpnu.ua>
2025-10-12 16:37:59 +0300

    Dz 6.2

--- lesson_6/index.js
+++ lesson_6/index.js
@@ -8,8 +8,20 @@ const user = {
   email: "Ihor@example.com",
 
   get info() {
-    return `Ім'я: ${user.name}\nAge: ${user.age}\nCity: ${user.city}\nEmail: ${user.email}`;
+    return `Name: ${user.name}\nAge: ${user.age}\nCity: ${user.city}\nEmail: ${user.email}`;
   }
 };
 
 console.log(user.info);
+
+
+// Дз 6.2
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbers = numbers.filter(function(element) {
+  return element % 2 === 0;
+});
+
+console.log("Парні числа:", evenNumbers);
+
