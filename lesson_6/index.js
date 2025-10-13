64fccd68f9bb3e8fa909f52b84e8a6995e45e1a3
Igor <ihor.diiak.fi.2022@lpnu.ua>
2025-10-13 17:30:49 +0300

    Dz 6.3

--- lesson_6/index.js
+++ lesson_6/index.js
@@ -17,11 +17,55 @@ console.log(user.info);
 
 // Дз 6.2
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const evenNumbers = numbers.filter(function(element) {
   return element % 2 === 0;
 });
 
 console.log("Парні числа:", evenNumbers);
+
+
+// Дз 6.3
+
+const phoneBook = {
+  contacts: [
+    {
+      name: "Oleg",
+      phone: "067-456-789",
+      email: "Oleg@gmail.com"
+    },
+    {
+      name: "Olya",
+      phone: "095-654-321",
+      email: "olya@egmail.com"
+    }
+  ],
+
+  findContact(name) {
+    const contact = phoneBook.contacts.find(function(element) {
+      return element.name === name;
+    });
+
+    if (contact) {
+      return contact;
+    } else {
+      return "Контакт не знайдено.";
+    }
+  },
+
+  addContact(newContact) {
+    phoneBook.contacts.push(newContact);
+  }
+};
+
+phoneBook.addContact({
+  name: "Ihor",
+  phone: "093-000-555",
+  email: "ihor@imail.com"
+});
+
+const found = phoneBook.findContact("Ihor");
+console.log(found);
+
 
