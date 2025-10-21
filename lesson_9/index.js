26a4d83a3cccef1faf6a86550c3a5e936c227a8f
Igor <ihor.diiak.fi.2022@lpnu.ua>
2025-10-21 18:47:36 +0300

    Dz 9.1

---
+++ lesson_9/index.js
@@ -0,0 +1,23 @@
+
+// ДЗ 9.1
+
+let ladder = {
+  ourstep: 0,
+
+  up: function() {
+    this. ourstep++;
+    return this; 
+  },
+
+  down: function() {
+    this. ourstep--;
+    return this; 
+  },
+
+  showStep: function() {
+    console.log(this. ourstep);
+    return this; 
+  }
+};
+
+ladder.up().up().down().showStep(); 
