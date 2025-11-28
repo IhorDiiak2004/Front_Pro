33d652e21bd808d0c76e26e2176ad6e7a164c5b3
Igor <ihor.diiak.fi.2022@lpnu.ua>
2025-11-28 15:23:10 +0200

    DZ 16_1 in constructor function

--- lesson_16_1/index.js
+++ lesson_16_1/index.js
@@ -1,49 +1,49 @@
 
-class Student {
-    constructor(firstName, lastName, birthYear) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthYear = birthYear;
-        this.grades = [];
-        this.attendance = [];
-        this.maxLessons = 25;
-    }
+function Student(firstName, lastName, birthYear) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthYear = birthYear;
+  this.grades = [];
+  this.attendance = [];
+  this.maxLessons = 25;
 
-    getAge(currentYear) {
-        return currentYear - this.birthYear;
-    }
+  this.getAge = function(currentYear) {
+    return currentYear - this.birthYear;
+  };
 
-    addGrade(grade) {
-        this.grades.push(grade);
-    }
+  this.addGrade = function(grade) {
+    this.grades.push(grade);
+  };
 
-    getAverageGrade() {
-        return this.grades.length ? this.grades.reduce((sum, grade) => sum + grade) / this.grades.length : 0;
-    }
+  this.getAverageGrade = function() {
+    if (!this.grades.length) return 0;
+    const sum = this.grades.reduce((acc, g) => acc + g, 0);
+    return sum / this.grades.length;
+  };
 
-    markAttendance(value) {
-        if (this.attendance.length < this.maxLessons) {
-            this.attendance.push(value);
-        } else {
-            console.log("Відвідуваність вже заповнена.");
-        }
+  this.markAttendance = function(value) {
+    if (this.attendance.length < this.maxLessons) {
+      this.attendance.push(value);
+    } else {
+      console.log("Відвідуваність вже заповнена.");
     }
+  };
 
-    present() {
-        this.markAttendance(true);
-    }
+  this.present = function() {
+    this.markAttendance(true);
+  };
 
-    absent() {
-        this.markAttendance(false);
-    }
+  this.absent = function() {
+    this.markAttendance(false);
+  };
 
-    summary() {
-        const avg = this.getAverageGrade();
-        const attendanceRate = this.attendance.filter(Boolean).length / this.attendance.length;
-        if (avg > 90 && attendanceRate > 0.9) return "Молодець!";
-        if (avg > 90 || attendanceRate > 0.9) return "Добре, але можна краще";
-        return "Редиска!";
-    }
+  this.summary = function() {
+    const avg = this.getAverageGrade();
+    const attendanceRate = this.attendance.filter(Boolean).length / this.attendance.length;
+    if (avg > 90 && attendanceRate > 0.9) return "Молодець!";
+    if (avg > 90 || attendanceRate > 0.9) return "Добре, але можна краще";
+    return "Редиска!";
+  };
 }
 
 const student1 = new Student("Іван", "Горм", 2000);
@@ -56,4 +56,5 @@ const student2 = new Student("Марія", "Петренко", 2004);
 student2.addGrade(92);
 student2.addGrade(91);
 student2.present();
-console.log(student2.summary());
+console.log(student2.summary()); 
+
