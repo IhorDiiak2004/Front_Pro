b3727d740bf787562df16160874a5728dd78f86a
Igor <ihor.diiak.fi.2022@lpnu.ua>
2025-11-28 13:48:34 +0200

    DZ 18

---
+++ lesson_18/index.js
@@ -0,0 +1,16 @@
+
+
+let time = 360; 
+const timer = document.getElementById("timer");
+
+const id = setInterval(() => {
+  const minutes = String(Math.floor(time / 60)).padStart(2, "0");
+  const seconds = String(time % 60).padStart(2, "0");
+
+  timer.textContent = `${minutes}:${seconds}`;
+
+  if (time-- <= 0) {
+    clearInterval(id);
+  }
+}, 1000);
+
